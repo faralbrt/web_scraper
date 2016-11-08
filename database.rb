aa6e5d2a00c2e50b2dfa7cd7d1d0215df9a88dfc
Albert Farhi <albert.farhi5@gmail.com>
2016-11-08 16:20:15 -0500

    add methods for interacting with database

--- database.rb
+++ database.rb
@@ -18,9 +18,40 @@ create_prices_cmd = <<-SQL
     asin VARCHAR(255),
     title VARCHAR(255),
     price_s VARCHAR(255),
-    price_i REAL 
+    price_i REAL,
+    day DATE
     )
 SQL
+
+# METHODS FOR asins table
+def add_asin(asin)
+  $db.execute("INSERT INTO asins (asin) VALUES (?)", [asin])
+end
+
+def delete_asin(asin)
+  $db.execute("DELETE FROM asins WHERE asin = ?", [asin])
+end
+
+def view_asins
+  $db.execute("SELECT asin FROM asins ORDER BY asin ASC")
+end
+
+# METHODS FOR PRICES TABLE
+def add_price(asin, title, price_s, price_i, day)
+  $db.execute("INSERT INTO prices (asin, title, price_s, price_i, day) VALUES (?,?,?,?)", [asin, title, price_s, price_i, day])
+end
+
+def view_prices
+  $db.execute("SELECT * FROM prices ORDER BY date DESC, asin ASC")
+end
+
+def search_by_date(day)
+  $db.execute("SELECT * FROM prices WHERE day=? ORDER BY asin ASC", [day])
+end
+
+def search_by_asin(asin)
+  $db.execute("SELECT * FROM prices WHERE asin= ? ORDER BY day DESC", [asin])
+end
 
 
 # DRIVER CODE
