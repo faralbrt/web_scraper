385f45839a664142a6e9fc6ed5cbf4385c20f8ee
Albert Farhi <albert.farhi5@gmail.com>
2016-11-09 19:34:02 -0500

    add graph to asin history page

--- sinatra.rb
+++ sinatra.rb
@@ -7,9 +7,15 @@ get '/' do
   erb :index
 end
 
-get '/:asin' do
+get '/history/:asin' do
   @asin = params[:asin]
   erb :asin
 end
 
+get '/addasin' do
+  "Hello"
+end
 
+get '/deleteasin' do
+  "Heyya!"
+end
